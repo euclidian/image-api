b1bf857681e8cc15fca136443af37650b9a5ad22
yunitanurjanah <yunitanurjanah7@gmail.com>
2019-07-11 10:24:04 +0700

    merubah url dari api/generateToken menjadi api/api/generateToken (#2)
    * merubah url dari api/generateToken menjadi api/api/generateToken
    
    * merubah routes

--- public/js/app.js
+++ public/js/app.js
@@ -81359,8 +81359,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/image-uploader-api/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/image-uploader-api/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /opt/lampp/htdocs/yunita/github/image-api/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /opt/lampp/htdocs/yunita/github/image-api/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
